86cc48f0a9a96300e3adc135e39f9695bcf0685b
Masagutov Kirill <dekerilljr@gmail.com>
2024-05-17 21:05:37 +0400

    Initial commit

---
+++ src/main/java/com/java/celosia/CelosiaApplication.java
@@ -0,0 +1,13 @@
+package com.java.celosia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelosiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelosiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/celosia/CelosiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.celosia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelosiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
