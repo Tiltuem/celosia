e55458e9ebe58a836d7329874b1f54d276869e49
Grigorii <osagrigoriy@mail.ru>
2024-05-19 00:44:36 +0400

    add sql

---
+++ src/main/resources/products.sql
@@ -0,0 +1,26 @@
+insert into product ("name", imagePath, weight, price, costPrice)
+values ("Ассорти пхали", "1.png", 240, 499, 105);
+
+insert into product ("name", imagePath, weight, price, costPrice)
+values ("Пхали из свеклы", "2.png", 80, 159, 35);
+
+insert into product ("name", imagePath, weight, price, costPrice)
+values ("Баклажаны по-грузински", "3.png", 150, 359, 55);
+
+insert into product ("name", imagePath, weight, price, costPrice)
+values ("ВАХ-Хачапури", "4.png", 1000, 999, 350);
+
+insert into product ("name", imagePath, weight, price, costPrice)
+values ("Хачапури на мангале", "5.png", 150, 279, 75);
+
+insert into product ("name", imagePath, weight, price, costPrice)
+values ("Хачапури по-Осетински", "6.png", 400, 339, 70);
+
+insert into product ("name", imagePath, weight, price, costPrice)
+values ("Хачапури по-Мегрельски", "7.png", 250, 359, 75);
+
+insert into product ("name", imagePath, weight, price, costPrice)
+values ("Стейк из семги", "8.png", 120, 659, 150);
+
+insert into product ("name", imagePath, weight, price, costPrice)
+values ("Королевские креветки", "9.png", 260, 999, 270);
