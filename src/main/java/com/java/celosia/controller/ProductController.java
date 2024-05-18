360aad6437156e4cc2445874dc6b827392338380
Grigorii <osagrigoriy@mail.ru>
2024-05-19 00:29:55 +0400

    add  super pupuer front

--- src/main/java/com/java/celosia/controller/ProductController.java
+++ src/main/java/com/java/celosia/controller/ProductController.java
@@ -9,9 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 
 @Controller
